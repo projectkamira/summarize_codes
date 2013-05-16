a7e75211d57a3f6d6d6c0ee220accaf75632d847
Marc Hadley <mhadley@mitre.org>
2013-05-16 11:57:11 -0400

    Add LOINC to SNOMED-CT mapping for vital signs

--- lib/summarize_codes.rb
+++ lib/summarize_codes.rb
@@ -8,6 +8,31 @@ module SummarizeCodes
   CODE_COLUMN = 4
   COUNT_COLUMN = 5
 
+  CODE_SYSTEM_ALIASES = {
+    'ICD9CM' => 'ICD-9-CM',
+    'ICD9PCS' => 'ICD-9-PCS',
+    'RXNORM' => 'RxNorm',
+    'SNOMEDCT' => 'SNOMED-CT'
+  }
+  
+  LOINC_TO_SNOMED = {
+    "8462-4" => "271650006",
+    "8480-6" => "271649006",
+    "8302-2" => "248327008",
+    "3141-9" => "107647005",
+    "8867-4" => "366199006",
+    "9279-1" => "366147009",
+    "8310-5" => "309646008"
+  }
+  
+  def self.oid_for_code_system_name(name)
+    name = CODE_SYSTEM_ALIASES[name] if CODE_SYSTEM_ALIASES[name]
+    HealthDataStandards::Util::CodeSystemHelper.oid_for_code_system(name)
+  end
+      
+  LOINC_OID = SummarizeCodes.oid_for_code_system_name('LOINC')
+  SNOMED_OID = SummarizeCodes.oid_for_code_system_name('SNOMED-CT')
+
   def self.validate_csv(rows)
     header = rows[HEADER_ROW]
     raise 'Did not find code system in column d' unless header[CODE_SYSTEM_COLUMN] == 'Code System'
@@ -23,19 +48,14 @@ module SummarizeCodes
       count = row[COUNT_COLUMN].to_i
       if count > 0
         summary[code_system_oid][code] = count
+        # Map LOINC vitals to SNOMED
+        if code_system_oid == LOINC_OID && LOINC_TO_SNOMED[code]
+          summary[SNOMED_OID] ||= {}
+          summary[SNOMED_OID][LOINC_TO_SNOMED[code]] = count
+        end
       end
     end
     summary
   end
 
-  CODE_SYSTEM_ALIASES = {
-    'ICD9CM' => 'ICD-9-CM',
-    'ICD9PCS' => 'ICD-9-PCS'
-  }
-  
-  def self.oid_for_code_system_name(name)
-    name = CODE_SYSTEM_ALIASES[name] if CODE_SYSTEM_ALIASES[name]
-    HealthDataStandards::Util::CodeSystemHelper.oid_for_code_system(name)
-  end
-      
 end
